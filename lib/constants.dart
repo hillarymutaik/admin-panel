a67781f134f1ef187a9c7c300332635b17b63c71
Hillary Mutai <mutaihillary01@gmail.com>
2022-07-17 10:09:25 +0300

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFF2697FF);
-const secondaryColor = Color(0xFF2A2D3E);
+const primaryColor = Color(0xff3fa0d7);
+const secondaryColor = Color(0xff29ee86);
 const bgColor = Color(0xFF212332);
+LinearGradient gradient = LinearGradient(colors: [
+  Color(0xff3fa0d7),
+  Color(0xff29ee86),
+]);
 
 const defaultPadding = 16.0;
